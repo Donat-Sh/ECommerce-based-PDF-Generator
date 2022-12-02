4962dafedcbe3af5fa63a120bdaed6d795298b30
Donat.Sh <45318518+Donat-Sh@users.noreply.github.com>
2022-12-02 13:54:04 +0200

    -Create interface to be used for the Service which shall generate the PDF.

---
+++ Services/Interfaces/IPdfGeneratorService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Domain;
+using System;
+
+namespace Services.Interfaces
+{
+    public interface IPdfGeneratorService
+    {
+        #region PdfGeneration
+
+        #region ConvertHtmlToPdf
+
+        Task<PdfOutput> ConvertHtmlToPdf(PdfInput pdfInput);
+
+        #endregion ConvertHtmlToPdf
+
+        #endregion PdfGeneration
+    }
+}
