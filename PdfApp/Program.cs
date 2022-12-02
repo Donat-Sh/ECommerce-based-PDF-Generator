b01fb7f97e7b8e32f69038ead19dbd5fc1924c0d
Donat.Sh <45318518+Donat-Sh@users.noreply.github.com>
2022-12-02 10:59:50 +0200

    -Add dev. notes & small Minimal API test case.

--- PdfApp/Program.cs
+++ PdfApp/Program.cs
@@ -5,11 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var app = builder.Build();
 
-app.Run();
-
-// Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+#region MinimalAPIs
 
 app.MapPost("/", (HttpRequest request, PdfInput? input) => {
     return Results.Ok(new PdfOutput(errorMessage: "To be implemented"));
 });
+
+app.MapGet("/", () => "Hello World!");
+
+#endregion MinimalAPIs
+
+app.Run();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
