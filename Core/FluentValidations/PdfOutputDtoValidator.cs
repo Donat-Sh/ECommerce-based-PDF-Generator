7d24b5b1648361bc52e54ce13b2a89f0b1d984a9
Donat.Sh <45318518+Donat-Sh@users.noreply.github.com>
2022-12-03 13:33:02 +0200

    -Add validation rules for 'PdfOutputDto.cs' class.

---
+++ Core/FluentValidations/PdfOutputDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using Core.Domain;
+using FluentValidation;
+
+namespace Core.FluentValidations
+{
+    public class PdfOutputDtoValidator : AbstractValidator<PdfOutputDto>
+    {
+        public PdfOutputDtoValidator()
+        {
+            #region Properties
+
+            RuleFor(x => x.PdfDocument).NotEmpty().WithMessage("OutPut .pdf document cannot be empty or null");
+            RuleFor(x => x.PdfDocumentSize).NotNull().GreaterThan(0).WithMessage("OutPut .pdf document size be null or valued 'negative/zero'");
+
+            #endregion Properties
+        }
+    }
+}
