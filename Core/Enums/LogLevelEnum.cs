8972a1ea178a71ab4c5741b30a2823dc2b4e3211
Donat.Sh <45318518+Donat-Sh@users.noreply.github.com>
2022-12-03 12:31:18 +0200

    -Implement the LogLevelEnum.

---
+++ Core/Enums/LogLevelEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+    public enum LogLevelEnum
+    {
+        Trace = 0,
+        Debug = 1,
+        Information = 2,
+        Warning = 3,
+        Error = 4,
+        Critical = 5,
+        Fatal = 6,
+        None = 7
+    }
+}
