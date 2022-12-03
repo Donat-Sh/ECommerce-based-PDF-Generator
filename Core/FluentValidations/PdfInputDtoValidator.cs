b787dea38fc68a0ac8cbf74eb2b0c78d2bddb95b
Donat.Sh <45318518+Donat-Sh@users.noreply.github.com>
2022-12-03 13:16:00 +0200

    -Implement the Validator for the 'PdfInputDto.cs' class.

---
+++ Core/FluentValidations/PdfInputDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using Core.Domain;
+using FluentValidation;
+
+namespace Core.FluentValidations
+{
+    public class PdfInputDtoValidator : AbstractValidator<PdfInputDto>
+    {
+        public PdfInputDtoValidator()
+        {
+            #region Properties
+
+            RuleFor(x => x.HtmlString).NotEmpty();
+
+            #endregion Properties
+
+            #region Complex Properties
+
+            RuleFor(x => x.Options).SetValidator(new PdfOptionsDtoValidator());
+
+            #endregion Complex Properties
+        }
+    }
+}
